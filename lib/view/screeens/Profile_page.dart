f1b680a536dda05c0706d48db4ce3a109daece92
aswin ashokan <aswinkannan1122@gmail.com>
2024-07-08 18:52:50 +0530

    start profile page

--- lib/view/screeens/Profile_page.dart
+++ lib/view/screeens/Profile_page.dart
@@ -1,11 +1,30 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_8/view/componenets/back_arrow.dart';
 
 class Profile extends StatelessWidget {
   const Profile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      
+      children: [
+        
+        
+        SizedBox(height: 50,width: 100,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            
+            BackArrow(ontapressed: (){}),
+            
+            Text("Profile",style: TextStyle(fontWeight: FontWeight.w900,fontSize: 28),),
+          
+            IconButton(onPressed: (){}, icon:Icon(Icons.settings,size: 28,))
+          ],
+        )],
+
+    );
   }
 }
